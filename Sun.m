38215e81391e1370ada44c9facea8b6a91fe3e09
Nick Dietrich <71101165+nckdietrich@users.noreply.github.com>
2021-03-26 15:13:39 -0600

    Update Sun.m

--- Sun.m
+++ Sun.m
@@ -4,7 +4,6 @@ classdef Sun
     (C) 2020 University of Colorado AES-CCAR-SEDA (Space Environment Data Analysis) Group 
     Written by Liam M. Kilcommons
     Translated to matlab code by Nick Dietrich
-    lil test edit
     %}
     methods (Static)
         function [jd] = datetime2jd(dt)
